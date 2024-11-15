b8e347970b495418146099c58729807d587f4635
matthewvbro101 <masteryblackcraft@gmail.com>
2024-11-15 12:10:15 -0800

    added logo

--- scoreboard/src/components/scoreboard/sb-row/row.tsx
+++ scoreboard/src/components/scoreboard/sb-row/row.tsx
@@ -1,24 +1,27 @@
+import { log } from "console";
 import "./row.css";
 
 interface RowData {
     pos: number,
+    logo: any,
     team: string,
     played: number,
     pts: number,
     gd: number
 }
 
-const Row = ({pos, team, played, pts, gd}: RowData) => {
+const Row = ({pos, logo, team, played, pts, gd}: RowData) => {
 
     
 
     return (
         <div className="rows">
-            <div className="row-pos num">{pos}</div>
+            <div className="row-pos">{pos}</div>
+            <img src={logo} className="row-logo" alt="Logo" />
             <div className="row-team">{team}</div>
-            <div className="row-played num">{played}</div>
-            <div className="row-pts num">{pts}</div>
-            <div className="row-gd num">{gd}</div>
+            <div className="row-played">{played}</div>
+            <div className="row-pts">{pts}</div>
+            <div className="row-gd">{gd}</div>
         </div>
     );
 }
--- scoreboard/src/components/scoreboard/scoreboard.tsx
+++ scoreboard/src/components/scoreboard/scoreboard.tsx
@@ -54,15 +54,15 @@ const Scoreboard = () => {
     return(
         <div className="scoreboard-wrap">
             <div className="sb-positions">
-                <div className="sb-pos num">#</div>
+                <div className="sb-pos">#</div>
                 <div className="sb-team">team</div>
-                <div className="sb-played num">PL</div>
-                <div className="sb-pts num">PTS</div>
-                <div className="sb-gd num">GD</div>
+                <div className="sb-played">PL</div>
+                <div className="sb-pts">PTS</div>
+                <div className="sb-gd">GD</div>
             </div>
            {standings && standings.map((teamData: any, index: number) => {
                 return(
-                    <Row key={index} pos={teamData.rank} team={teamData.team.name} played={teamData.all.played} pts={teamData.points} gd={teamData.goalsDiff}/>
+                    <Row key={index} pos={teamData.rank} logo={teamData.team.logo} team={teamData.team.name} played={teamData.all.played} pts={teamData.points} gd={teamData.goalsDiff}/>
                 );
            })}
             
