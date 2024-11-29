11ad9243864197b6ad240f1bbccba66157f9688d
matthewvbro101 <masteryblackcraft@gmail.com>
2024-11-29 11:42:14 -0800

    Update topLeagues.tsx

--- scoreboard/src/components/topLeagues/topLeagues.tsx
+++ scoreboard/src/components/topLeagues/topLeagues.tsx
@@ -19,13 +19,11 @@ const TopLeagues = () => {
 
       let leagueNames = ['Premier League','Champions League', 'Bundesliga', 'Europa League', 'Ligue 1', 'Serie A' ,  'MLS', 'LaLiga'];
     //   league number 2: Champ League, 39: Prem League, 78: Bundesliga, 3: Europa League, 61: Ligue 1, 140: laliga, 253: mls, 135: serie A
-      const [leagueNumID, setLeagueNumID] = useState([39, 2, 78, 3, 61, 135, 253, 140]);
+      let leagueNum = [39, 2, 78, 3, 61, 135, 253, 140];
 
-      const handleClick = async () => {
+      const handleClick = (num: number[]): number[] => {
         // await new ApiService().getScoreBoard(leagueNumID[index])
-        leagueNumID.map((item, index) => {
-            
-          });
+        return num.map((item) => item);
       }
 
     useEffect(() => {
@@ -82,7 +80,7 @@ const TopLeagues = () => {
             </thead>
             <tbody>
                 {leagueNames.map((item, index) => (
-                    <tr key={index} className="league-row" >
+                    <tr key={index} className="league-row" onClick={() => {handleClick(leagueNum)}}                                                                                                                                                >
                         <td className="logo-wrap">
                             <img src={leagueLogos[item.toLowerCase().replace(/\s+/g, '')]}  className="logo" alt="logo"></img>
                         </td>
