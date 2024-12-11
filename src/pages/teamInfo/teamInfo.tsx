e18a7762624cca081d6805da98a96e1abd8a9928
matthewvbro101 <masteryblackcraft@gmail.com>
2024-12-10 16:45:21 -0800

    team information

--- src/pages/teamInfo/teamInfo.tsx
+++ src/pages/teamInfo/teamInfo.tsx
@@ -1,17 +1,23 @@
 import HeadlineLeague from "../../components/headlineLeague/headlineLeague";
-import { useLocation } from "react-router-dom";
+import {useLocation } from "react-router-dom";
 import "./teamInfo.css";
 import Scoreboard from "../../components/scoreboard/scoreboard";
+import Form from "../../components/scoreboard/form/form";
 
 const TeamInfo = () =>  {
 
     const location = useLocation();
     const { pos, logo, team, played, pts, gd, tournament, form } = location.state || {};
 
+    console.log(team);
 
     return (
         <div className="teamInfo-wrap">
             <HeadlineLeague leagueNum="39" />
+            <div className="team-form panel">
+                <span className="team-form-txt">Team Form</span>
+                <Form form={form}/>
+            </div>
             <Scoreboard leagueNum="39" />
 
         </div>
